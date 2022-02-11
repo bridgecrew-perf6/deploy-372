c0ca118c1d966d2301866598d98895373f53a717
Hiren Parghi <hirenparghi90@gmail.com>
2022-02-11 19:56:49 +0530

    123

--- handler.js
+++ handler.js
@@ -5,7 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'This is Function 1!',
+        message: 'Go Serverless v1.0! Your function executed successfully!',
         input: event,
       },
       null,
