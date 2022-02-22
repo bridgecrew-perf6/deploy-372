d29ec9dcf06e23d5f39a58a9ca36b31df66d6bb3
Hiren Parghi <hirenparghi90@gmail.com>
2022-02-22 19:07:48 +0530

    07:07

--- src3/index.js
+++ src3/index.js
@@ -1,9 +1,7 @@
 const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
- 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+res.send('You successfully called your first Lambda function!')
 })
- 
 module.exports.handler = serverless(app);
